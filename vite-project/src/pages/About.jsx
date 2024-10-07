05dffa17c503279018cc60781c3bf0a76ac2bb30
Soumili <soumilimukherjee20@gmail.com>
2024-10-08 03:07:50 +0530

    about half done

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,9 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
 
 const About = () => {
   return (
-    <div></div>
+    <div>
+
+      <div>
+        <p>ABOUT <span>US</span></p>
+      </div>
+      <div>
+        <img src= {assets.about_image} alt="" />
+        <div>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
